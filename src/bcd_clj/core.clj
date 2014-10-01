e3494ea5d8fa0f220af93bc5ec0c542993c557a9
FScoward <endlick1989@yahoo.co.jp>
2014-10-02 00:38:16 +0900

    書き直し

--- src/bcd_clj/core.clj
+++ src/bcd_clj/core.clj
@@ -1,39 +1,18 @@
-;; https://gist.github.com/neuro-sys/953548 の clojure 実装
 (ns bcd-clj.core)
 
 ; 桁数計算
 (def counter (ref 0))
 (defn calc-digits [num]
-  (dosync (alter counter inc))
-  (def result (/ num 10))
-  (if (>= result 1) (calc-digits result) counter))
+(dosync (alter counter inc))
+(def result (quot num 10))
+(if (>= result 1) (calc-digits result) counter)
+  (deref counter))
 
-(defn dec-to-bcd-array [num]
-  ; 桁数
-  (def digit (deref (calc-digits num)))
+(defn toBCD [num]
+  (def digits (calc-digits num))
+  (def str-array
+    (for [x (range digits)]
+      (Integer/toBinaryString (int (.charAt (.toString num) x)))))
+  (map (fn [x] (subs x 2)) str-array))
 
-  (def byte-len (if (= 0 (rem digit 2))
-                  (/ digit 2)
-                  (/ (+ digit 1) 2)))
-  ; 奇数か？
-  (def isOdd (odd? num))
-
-  (def bcd (byte-array byte-len))
-
-  (loop [i 0]
-    (when (< i digit)
-      (let [tmp (rem num 10)]
-        ; todo
-        (if (and (= i (- digit 1)) isOdd)
-          (aset-byte bcd (/ i 2) tmp)
-          (if (= 0 (/ i 2))
-            (aset-byte bcd (/ i 2) tmp)
-            (aset-byte bcd (/ i 2) (bit-shift-left tmp 4))
-            ))
-        (recur (inc i)))))
-  (vec bcd)
-  )
-
-
-
-(dec-to-bcd-array 123)
+(toBCD 123)
